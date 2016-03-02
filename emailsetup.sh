af1936e79685eb62c720ab15c7439facb25f4e00
Dan Shuman <StarshipEngineer@users.noreply.github.com>
2016-03-02 17:08:24 -0600

    Added files

---
+++ emailsetup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install postfix
+
+cd /etc/postfix/
+sed -i -e "\$ainet_protocols = ipv4" main.cf
+
+# enter hostname and alter main to have it
+NAME=$(whiptail --inputbox "Enter your fully-qualified domain name (ex example.com):" \
+8 78 --title "Setup OpenVPN" 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+ whiptail --title "Domain Name" --infobox "Domain: $NAME" 8 78
+else
+ whiptail --title "Domain Name" --infobox "Cancelled" 8 78
+ exit
+fi
+
+sed -i '/myhostname/cmyhostname = $NAME' main.cf
+
+service postfix restart
---
+++ test.sh
@@ -0,0 +1,11 @@
+NAME=$(whiptail --inputbox "Enter your fully-qualified domain name (ex example.com):" \
+8 78 --title "Setup OpenVPN" 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+ whiptail --title "Domain Name" --infobox "Domain: $NAME" 8 78
+else
+ whiptail --title "Domain Name" --infobox "Cancelled" 8 78
+ exit
+fi
+
+sed -i '/myhostname/cmyhostname = $NAME' main.cf
