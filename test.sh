d1b0ec5066980d5039fbb156db41bec1940358fa
Dan Shuman <dan.13.shuman@gmail.com>
2016-03-03 13:03:49 -0600

    Updates test script

--- test.sh
+++ test.sh
@@ -1,12 +1,5 @@
-NAME=$(whiptail --inputbox "Enter your fully-qualified domain name (ex example.com):" \
-8 78 --title "Setup OpenVPN" 3>&1 1>&2 2>&3)
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
- whiptail --title "Domain Name" --infobox "Domain: $NAME" 8 78
-else
- whiptail --title "Domain Name" --infobox "Cancelled" 8 78
- exit
-fi
 
 cd /etc/postfix/
-sed -i '/myhostname/cmyhostname = '$NAME'' main.cf
+
+sed -i -e "\$ahome_mailbox = Maildir/" main.cf
+sed -i -e "\$amailbox_command =" main.cf
