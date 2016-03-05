d51cef0a53b3ed3e4d2a98ff578da3daee815b32
Dan Shuman <dan.13.shuman@gmail.com>
2016-03-04 21:22:25 -0600

    Updates test code

--- test.sh
+++ test.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if (whiptail --title "Test Yes/No Box" --yesno "Choose between Yes and No." 10 60) then
+
+    echo "You chose Yes. Exit status was $?."
+
+else
+
+    echo "You chose No. Exit status was $?."
+
+fi
+
+
+
 # recursion test
 exitstatus=0
 while [ $exitstatus = 0 ]
