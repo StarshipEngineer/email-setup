8fe38b4ce6821be3e139146637c782dd93111acb
Dan Shuman <dan.13.shuman@gmail.com>
2016-03-07 13:32:24 -0600

    Updates test code

--- emailsetup.sh
+++ emailsetup.sh
@@ -4,8 +4,35 @@
 chmod +x setup.sh
 chmod +x adduser.sh
 
+exitstatus=0
+while [ $exitstatus = 0 ]
+do
+	CHOICE=$(whiptail --title "Recursion test" --menu "Do you want to recur?" 16 78 3 \
+	"01" "Setup email" \
+	"02" "Add an existing user" \
+	"03" "Add a new user" \
+	"04" "Remove email" 3>&1 1>&2 2>&3)
 
-#!/bin/bash
+	case "${CHOICE}" in
+		01)
+		 #command
+		 whiptail --title "Setup email" --infobox "1" 8 78
+		;;
+		02)
+		 whiptail --title "Add an existing user" --infobox "2" 8 78
+		;;
+		03)
+		 whiptail --title "Add a new user" --infobox "3" 8 78
+		;;
+		04)
+		 whiptail --title "Remove email" --infobox "4" 8 78
+		;;
+		*)
+			exitstatus=1
+			exit
+		;;
+	esac
+done
 
 askreboot() {
 	if (whiptail --title "Reboot" --yesno --defaultno "Would you like to reboot now?" 8 78) then
--- test.sh
+++ test.sh
@@ -1,38 +1,3 @@
 #!/bin/bash
 
-if (whiptail --title "Test Yes/No Box" --yesno "Choose between Yes and No." 10 60) then
 
-    echo "You chose Yes. Exit status was $?."
-
-else
-
-    echo "You chose No. Exit status was $?."
-
-fi
-
-
-
-# recursion test
-# is formal recursion necessary? This just kicks you back to main screen anyway
-exitstatus=0
-while [ $exitstatus = 0 ]
-do
-	CHOICE=$(whiptail --title "Recursion test" --menu "Do you want to recur?" 16 78 3 \
-	"01" "Yes - Recur" \
-	"02" "No - Exit" 3>&1 1>&2 2>&3)
-
-	case "${CHOICE}" in
-		01)
-		 #command
-  whiptail --title "Recursion test" --infobox "You have recurred." 8 78
-		;;
-		02)
-  exitstatus=1
-		 exit
-		;;
-		*)
-			exitstatus=1
-			exit
-		;;
-	esac
-done
