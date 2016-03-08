b63b8d5cdbd863a9e7c26bcdca4e10924dbc8f98
Dan Shuman <dan.13.shuman@gmail.com>
2016-03-08 17:07:11 -0600

    Updates test code

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 
+maildirmake.dovecot /etc/skel/Maildir
+maildirmake.dovecot /etc/skel/Maildir/.Drafts
+maildirmake.dovecot /etc/skel/Maildir/.Sent
+maildirmake.dovecot /etc/skel/Maildir/.Spam
+maildirmake.dovecot /etc/skel/Maildir/.Trash
+maildirmake.dovecot /etc/skel/Maildir/.Templates
