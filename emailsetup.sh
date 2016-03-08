7df5f848421f00f3a513d81e204af387a6358322
Dan Shuman <dan.13.shuman@gmail.com>
2016-03-07 21:41:55 -0600

    Updates front-end script

--- emailsetup.sh
+++ emailsetup.sh
@@ -1,39 +1,5 @@
 #!/bin/bash
 
-# Make component scripts executable
-chmod +x setup.sh
-chmod +x adduser.sh
-
-exitstatus=0
-while [ $exitstatus = 0 ]
-do
-	CHOICE=$(whiptail --title "Recursion test" --menu "Do you want to recur?" 16 78 3 \
-	"01" "Setup email" \
-	"02" "Add an existing user" \
-	"03" "Add a new user" \
-	"04" "Remove email" 3>&1 1>&2 2>&3)
-
-	case "${CHOICE}" in
-		01)
-		 #command
-		 whiptail --title "Setup email" --infobox "1" 8 78
-		;;
-		02)
-		 whiptail --title "Add an existing user" --infobox "2" 8 78
-		;;
-		03)
-		 whiptail --title "Add a new user" --infobox "3" 8 78
-		;;
-		04)
-		 whiptail --title "Remove email" --infobox "4" 8 78
-		;;
-		*)
-			exitstatus=1
-			exit
-		;;
-	esac
-done
-
 askreboot() {
 	if (whiptail --title "Reboot" --yesno --defaultno "Would you like to reboot now?" 8 78) then
 		whiptail --title "Reboot" --msgbox "The system will reboot." 8 78
@@ -41,27 +7,36 @@ askreboot() {
 	fi
 }
 
+# Make component scripts executable
+chmod +x setup.sh
+chmod +x adduser.sh
 
 exitstatus=0
 while [ $exitstatus = 0 ]
 do
-	CHOICE=$(whiptail --title "OpenVPN-Setup" --menu "Setup options:" 16 78 3 \
-	"01" "Setup OpenVPN on your system" \
-	"02" "Generate a client profile with MakeOVPN" \
-	"03" "Remove OpenVPN and revert your system to a pre-installation state" 3>&2 2>&1 1>&3)
+	CHOICE=$(whiptail --title "Email setup" --menu "Setup options:" 16 78 3 \
+	"01" "Setup email" \
+	"02" "Add an existing user" \
+	"03" "Add a new user" \
+	"04" "Remove email" 3>&1 1>&2 2>&3)
+
+# add options for setting up components and full install
 
 	case "${CHOICE}" in
 		01)
-			sudo chmod +x install.sh
-			sudo ./install.sh
-			askreboot
+		 #command
+		 whiptail --title "Setup email" --infobox "1" 8 78
+  ./setup.sh
 		;;
 		02)
-			./MakeOVPN.sh
+		 whiptail --title "Add an existing user" --infobox "2" 8 78
+  #./adduser.sh
 		;;
 		03)
-			sudo ./remove.sh
-			askreboot
+		 whiptail --title "Add a new user" --infobox "3" 8 78
+		;;
+		04)
+		 whiptail --title "Remove email" --infobox "4" 8 78
 		;;
 		*)
 			exitstatus=1
