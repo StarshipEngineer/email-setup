02d314a40f1d6af5021a0819d121d0f04be2615d
Dan Shuman <dan.13.shuman@gmail.com>
2016-03-09 21:57:54 -0600

    Updates config template

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 
 sed -i -e "\$asmtpd_recipient_restrictions = permit_sasl_authenticated, permit_mynetworks, reject_unauth_destination"
 
-#cat<< EOF >>
+#cd /etc/postfix
+#cat /home/pi/email-setup/config.txt>>main.cf
