f88bf4073a0357cb46d30048dbe95824c755767b
Dan Shuman <dan.13.shuman@gmail.com>
2016-03-10 07:00:49 -0600

    Updates test code

--- test.sh
+++ test.sh
@@ -2,12 +2,12 @@
 
 cd /etc/postfix
 
-sed -i -e "\$asmtpd_recipient_restrictions =/
-    permit_sasl_authenticated,/
-    permit_mynetworks,/
-    reject_unauth_destination" main.cf
+#sed -i -e "\$a/
+    #smtpd_recipient_restrictions =
+    #permit_sasl_authenticated,
+    #permit_mynetworks,
+    #reject_unauth_destination" main.cf
 
 service postfix reload
 
-#cd /etc/postfix
-#cat /home/pi/email-setup/config.txt>>main.cf
+cat /home/pi/email-setup/config.txt>>main.cf
