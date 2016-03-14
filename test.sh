4988e130d74c0b984df3b7a0b21c3a9352e3a62e
Dan Shuman <dan.13.shuman@gmail.com>
2016-03-13 20:48:18 -0500

    Updates test code

--- test.sh
+++ test.sh
@@ -8,12 +8,12 @@ cp /etc/dovecot/conf.d/10-master.conf /etc/dovecot/conf.d/10-master.conf.BAK
 
 cp /etc/dovecot/conf.d/10-auth.conf /etc/dovecot/conf.d/10-auth.conf.BAK
 
-sed -i -e '/mail_location =/cmail_location = maildir:~/Maildir' /etc/dovecot/conf.d/10-mail.conf
+sed -i '/mail_location =/cmail_location = maildir:~/Maildir' /etc/dovecot/conf.d/10-mail.conf
 
-sed -i -e '/unix_listener auth-userdb {/cunix_listener /var/spool/postfix/private/auth {'
-sed -i -e '/#mode/cmode = 0660' /etc/dovecot/conf.d/10-master.conf
-sed -i -e '/#user/cuser = postfix' /etc/dovecot/conf.d/10-master.conf
-sed -i -e '/#group/cgroup = postfix' /etc/dovecot/conf.d/10-master.conf
+sed -i '/unix_listener auth-userdb {/cunix_listener /var/spool/postfix/private/auth {' /etc/dovecot/conf.d/10-master.conf
+sed -i '/#mode/cmode = 0660' /etc/dovecot/conf.d/10-master.conf
+sed -i '/#user/cuser = postfix' /etc/dovecot/conf.d/10-master.conf
+sed -i '/#group/cgroup = postfix' /etc/dovecot/conf.d/10-master.conf
 
-sed -i -e '/disable_plaintext_auth =/cdisable_plaintext_auth= no' /etc/dovecot/conf.d/10-auth.conf
-sed -i -e '/auth_mechanisms =/cauth_mechanisms = plain login' /etc/dovecot/conf.d/10-auth.conf
+sed -i '/disable_plaintext_auth =/cdisable_plaintext_auth= no' /etc/dovecot/conf.d/10-auth.conf
+sed -i '/auth_mechanisms =/cauth_mechanisms = plain login' /etc/dovecot/conf.d/10-auth.conf
