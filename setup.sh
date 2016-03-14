ab9bf010370e72b52f405e9c12f81cb4e82a3207
Dan Shuman <dan.13.shuman@gmail.com>
2016-03-13 22:56:18 -0500

    Updates setup script

--- setup.sh
+++ setup.sh
@@ -58,5 +58,22 @@ sed -i -e '/#listen =/clisten = *' dovecot.conf
 
 service dovecot restart
 
+cp /etc/dovecot/conf.d/10-mail.conf /etc/dovecot/conf.d/10-mail.conf.BAK
+
+cp /etc/dovecot/conf.d/10-master.conf /etc/dovecot/conf.d/10-master.conf.BAK
+
+cp /etc/dovecot/conf.d/10-auth.conf /etc/dovecot/conf.d/10-auth.conf.BAK
+
+sed -i ':mail_location = mbox:~/mail:INBOX=/var/mail/%u:cmail_location = maildir:~/Maildir' /etc/dovecot/conf.d/10-mail.conf
+
+sed -i ':unix_listener auth-userdb {:cunix_listener /var/spool/postfix/private/auth {' /etc/dovecot/conf.d/10-master.conf
+sed -i '/#mode/cmode = 0660' /etc/dovecot/conf.d/10-master.conf
+sed -i '/#user/cuser = postfix' /etc/dovecot/conf.d/10-master.conf
+sed -i '/#group/cgroup = postfix' /etc/dovecot/conf.d/10-master.conf
+
+sed -i '/disable_plaintext_auth =/cdisable_plaintext_auth= no' /etc/dovecot/conf.d/10-auth.conf
+sed -i '/auth_mechanisms =/cauth_mechanisms = plain login' /etc/dovecot/conf.d/10-auth.conf
+
+
 #currently here
 
--- test.sh
+++ test.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
 #cd /etc/dovecot
-
-cp /etc/dovecot/conf.d/10-mail.conf /etc/dovecot/conf.d/10-mail.conf.BAK
-
-cp /etc/dovecot/conf.d/10-master.conf /etc/dovecot/conf.d/10-master.conf.BAK
-
-cp /etc/dovecot/conf.d/10-auth.conf /etc/dovecot/conf.d/10-auth.conf.BAK
-
-sed -i '/mail_location =/cmail_location = maildir:~/Maildir' /etc/dovecot/conf.d/10-mail.conf
-
-sed -i '/unix_listener auth-userdb {/cunix_listener /var/spool/postfix/private/auth {' /etc/dovecot/conf.d/10-master.conf
-sed -i '/#mode/cmode = 0660' /etc/dovecot/conf.d/10-master.conf
-sed -i '/#user/cuser = postfix' /etc/dovecot/conf.d/10-master.conf
-sed -i '/#group/cgroup = postfix' /etc/dovecot/conf.d/10-master.conf
-
-sed -i '/disable_plaintext_auth =/cdisable_plaintext_auth= no' /etc/dovecot/conf.d/10-auth.conf
-sed -i '/auth_mechanisms =/cauth_mechanisms = plain login' /etc/dovecot/conf.d/10-auth.conf
