7942d34eafceeb9c229dcd0c02aa5c37681de40d
Dan Shuman <dan.13.shuman@gmail.com>
2016-03-14 22:07:33 -0500

    Updates test code

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i '\$asmtp_sasl_auth_enable = yes' /etc/postfix/main.cf
-sed -i '\$asmtpd_sasl_auth_enable = yes' /etc/postfix/main.cf
+sed -i -e '\$asmtp_sasl_auth_enable = yes' /etc/postfix/main.cf
+sed -i -e '\$asmtpd_sasl_auth_enable = yes' /etc/postfix/main.cf
