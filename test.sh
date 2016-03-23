c2bc8e7283be63b1687cdc1771596e5694865640
Dan Shuman <dan.13.shuman@gmail.com>
2016-03-22 19:34:29 -0500

    Updates test code

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sed -i "/#smtps/csmtps inet n - - - - smtpd" /etc/postfix/master.cf
+
+sed -i "/syslog_name=postfix/c-o syslog_name=postfix/smtps" /etc/postfix/master.cf
+sed -i "/smtpd_tls_wrappermode=yes/c-o smtpd_tls_wrappermode=yes" /etc/postfix/master.cf
