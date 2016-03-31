9dce14d0bc580dbd8734337c1cb73ba69f2a6877
Dan Shuman <dan.13.shuman@gmail.com>
2016-03-31 06:16:08 -0500

    Updates test code

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#sed -i "29c \  -o syslog_name=postfix/smtps" /etc/postfix/master.cf
-sed -i "31i \  -o smtpd_recipient_restrictions=permit_sasl_authenticated,reject" /etc/postfix/master.cf
+cat /home/pi/email-setup/10-master.txt>>/etc/dovecot/conf.d/10-master.conf
